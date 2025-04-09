088e1b847991eb6ceea2f0040f844a6b33ca3ef5
GloriaAndonyaIT <gloriaandonyaa@gmail.com>
2025-04-09 23:00:10 +0300

    Complete lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,19 @@
 import React from "react";
 
-function NavBar() {
-  // update the JSX being returned!
-  return <nav>NavBar</nav>;
-}
+
+
+  function NavBar() {
+    // update the JSX being returned!
+    return <nav>
+      <a href ="#home">Home</a>
+      <a href ="#about">About</a>
+  
+  
+     
+  
+  
+    </nav>;
+  
+  }
 
 export default NavBar;
